3a4da5747463186671bba19f7cda9361bdae6f0d
Guillermo <ggjnez92@gmail.com>
2021-10-01 11:25:45 +0200

    Fix perlin noise implementation

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 let ctx;
-const width = 200;
-const height = 200;
+let vai
+const width = 1920;
+const height = 1080;
 
 const globalMod = ~(1 << 31);
-let globalSeed = new Date().getTime();
+let globalSeed = 1633043671972 || new Date().getTime();
 console.log('global seed:', globalSeed);
 
+let cont = 0;
 let xxx = 0;
 let sxx = 0.01;
 let sx = 1;
@@ -16,7 +18,7 @@ function readConfig() {
   ctx.canvas.width = width;
   ctx.canvas.height = height;
 
-  const vai = document.getElementById("value");
+  vai = document.getElementById("value");
   vai.value = sxx + "";
   vai.onchange = () => {
     sxx = +vai.value;
@@ -65,8 +67,6 @@ function partialRand(globalSeed, localSeed) {
   return [...new Array(iterations)].fill(null).reduce(() => r());
 }
 
-window.a = 0;
-
 const noiseLevelDiff = 2;
 
 function getNoiseSeed(seed, level = 5) {
@@ -74,7 +74,6 @@ function getNoiseSeed(seed, level = 5) {
   const levels = [...new Array(level)]
     .fill(null)
     .map((ignore, i) => Math.pow(noiseLevelDiff, -i));
-  window.a < 2 && console.log(levels);
   const getBorders = (time, size) => {
     let l = Math.floor(time / size) * size;
     return {l, r: l + size};
@@ -92,38 +91,24 @@ function getNoiseSeed(seed, level = 5) {
     let left = borders.l, right = borders.r;
     let vl = evalTime(left), vr = evalTime(right);
 
-    window.a < 2 && console.log([...new Array(level - 1)]
-      .fill(null)
-      .map((ignore, i) => 1 / Math.pow(5, i + 1)));
-    window.a < 2 && console.log('time', time);
     for (let i = 1; i < levels.length; i++) {
       let {l, r} = getBorders(time, levels[i])
 
       let tvl = vl, tvr = vr;
-      const w = 1 / Math.pow(5, i + 1);
-      window.a < 2 && console.log('ll', l, left);
+      const w = 1 / Math.pow(2, i);
       if (l !== left) {
+        tvl = evalInterval(left, right, l, vl, vr) + evalTime(l) * w - w / 2;
         left = l;
-        tvl = evalInterval(left, right, l, vl, vr) + evalTime(left) * w - w / 2;
-        window.a < 4 && console.log('-l-', evalTime(left) * w - w / 2, w / 2);
       }
-      window.a < 2 && console.log('rr', r, right);
       if (r !== right) {
+        tvr = evalInterval(left, right, r, vl, vr) + evalTime(r) * w - w / 2;
         right = r;
-        tvr = evalInterval(left, right, r, vl, vr) + evalTime(right) * w - w / 2;
-        window.a < 4 && console.log('-r-', evalTime(left) * w - w / 2, w / 2);
       }
-      // console.log(time, left, right, levels[i])
       vl = Math.min(1, Math.max(0, tvl));
       vr = Math.min(1, Math.max(0, tvr));
     }
-    window.a++;
     return Math.min(1, Math.max(0, evalInterval(left, right, time, vl, vr)));
   };
-}
-
-for (let i = 0, j = 0; i < 20; i++, j += sxx) {
-  console.log(i, partialRand(globalSeed, i));
 }
 
 function calc(arr, p, limit) {
@@ -137,96 +122,111 @@ function calc(arr, p, limit) {
 }
 
 function render() {
-  ctx.beginPath();
-  ctx.fillStyle = "#ffffff";
-  ctx.rect(0, 0, width, height);
-  ctx.fill();
+  if (!window.x) {
+    ctx.beginPath();
+    ctx.fillStyle = "#ffffff";
+    ctx.rect(0, 0, width, height);
+    ctx.fill();
+  }
+  window.x = true;
 
   // noise
-  const points = [];
-  for (let i = 0; i < width; i++) {
-    points.push(null);
-  }
-  let j = 40;
-  for (let i = 0; i < width; i += j) {
-    points[i] = partialRand(globalSeed, i) * height;
-  }
-  points[points.length - 1] = partialRand(globalSeed, points.length - 1) * height;
-  for (let i = 0, l = 2; i < 3; i++, l++) {
-    j = Math.floor(j / 5);
-    for (let k = 0; k < width && j > 0; k += j) {
-      const size = height / Math.pow(4, l);
-      let curr = partialRand(globalSeed, k) * size - size / 2;
-      if (points[k] === null) {
-        points[k] = calc(points, k, width) + curr;
-      }
-      points[k] = Math.min(height, Math.max(0, points[k]));
-    }
-  }
-
-  for (let i = 0; i < width; i++) {
-    if (points[i] === null) {
-      points[i] = calc(points, i, width);
-    }
-  }
-
-  let x = 0;
-  ctx.beginPath();
-  ctx.strokeStyle = "#000";
-  ctx.strokeWidth = "0.5px";
-  ctx.moveTo(0, map(points[x], 0, height, height / 2, height));
-  while (x <= width) {
-    x += 2;
-    ctx.lineTo(x, map(points[x], 0, height, height / 2, height));
-  }
-  ctx.stroke();
+  // const points = [];
+  // for (let i = 0; i < width; i++) {
+  //   points.push(null);
+  // }
+  // let j = 40;
+  // for (let i = 0; i < width; i += j) {
+  //   points[i] = partialRand(globalSeed, i) * height;
+  // }
+  // points[points.length - 1] = partialRand(globalSeed, points.length - 1) * height;
+  // for (let i = 0, l = 2; i < 3; i++, l++) {
+  //   j = Math.floor(j / 5);
+  //   for (let k = 0; k < width && j > 0; k += j) {
+  //     const size = height / Math.pow(4, l);
+  //     let curr = partialRand(globalSeed, k) * size - size / 2;
+  //     if (points[k] === null) {
+  //       points[k] = calc(points, k, width) + curr;
+  //     }
+  //     points[k] = Math.min(height, Math.max(0, points[k]));
+  //   }
+  // }
+  //
+  // for (let i = 0; i < width; i++) {
+  //   if (points[i] === null) {
+  //     points[i] = calc(points, i, width);
+  //   }
+  // }
+  //
+  // let x = 0;
+  // ctx.beginPath();
+  // ctx.strokeStyle = "#000";
+  // ctx.strokeWidth = "0.5px";
+  // ctx.moveTo(0, map(points[x], 0, height, height / 2, height));
+  // while (x <= width) {
+  //   x += 2;
+  //   ctx.lineTo(x, map(points[x], 0, height, height / 2, height));
+  // }
+  // ctx.stroke();
 
   // drawMarkers("#000dff", Math.pow(noiseLevelDiff, -3))
   // drawMarkers("#ffc400", Math.pow(noiseLevelDiff, -2))
-  drawMarkers("#f00", Math.pow(noiseLevelDiff, -1))
-  drawMarkers("#0f0", Math.pow(noiseLevelDiff, 0))
+  // drawMarkers("#f00", Math.pow(noiseLevelDiff, -1))
+  // drawMarkers("#0f0", Math.pow(noiseLevelDiff, 0))
 
-  const n = getNoiseSeed(globalSeed, 5);
-  x = 0;
+  // const n = getNoiseSeed(globalSeed, 5);
+  // x = 0;
+  // ctx.beginPath();
+  // ctx.strokeStyle = "#000000";
+  // ctx.strokeWidth = "1px";
+  // let xx = xxx;
+  // ctx.moveTo(0, map(n(xx), 0, 1, 0, height / 2));
+  // while (x <= width) {
+  //   x += sx;
+  //   xx += sxx;
+  //   ctx.lineTo(x, map(n(xx), 0, 1, 0, height / 2));
+  // }
+  // ctx.stroke();
+
+
+  const n = getNoiseSeed(globalSeed, 6);
+
   ctx.beginPath();
-  ctx.strokeStyle = "#000000";
-  ctx.strokeWidth = "1px";
-  let xx = xxx;
-  ctx.moveTo(0, map(n(xx), 0, 1, 0, height / 2));
-  while (x <= width) {
-    x += sx;
-    xx += sxx;
-    ctx.lineTo(x, map(n(xx), 0, 1, 0, height / 2));
-  }
+  ctx.strokeStyle = "rgba(28,50,80,0.05)";
+  ctx.lineWidth = 1200;
+  ctx.moveTo(map(n(xxx), 0, 1, 0, width), map(n(xxx + 10), 0, 1, 0, height));
+  xxx += 0.5;
+  ctx.lineTo(map(n(xxx), 0, 1, 0, width), map(n(xxx + 10), 0, 1, 0, height));
   ctx.stroke();
-  // xxx += 0.000001;
-}
 
-function drawMarkers(color, step) {
-  let x = 0;
-  let xx = xxx;
-  while (x <= width) {
-    const ps = map(xx, 0, sxx, 0, sx);
-    x = ps;
-    ctx.beginPath();
-    ctx.strokeStyle = color;
-    ctx.strokeWidth = ".1px";
-    ctx.moveTo(ps, 0);
-    ctx.lineTo(ps, height);
-    ctx.stroke();
-    xx += step;
-  }
+  cont++;
+  vai.value = cont;
+
+  // let xoff = 0, yoff = 0;
+  // for (let i = 0; i < width / 2; i++) {
+  //   xoff = 0;
+  //   for (let j = 0; j < height / 2; j++) {
+  //     const col = Math.floor(map(n(xoff * yoff), 0, 1, 0, 255));
+  //     ctx.beginPath()
+  //     ctx.fillStyle = `#${col.toString(16)}${col.toString(16)}${col.toString(16)}ff`;
+  //     ctx.rect(i * 2, j * 2, 2, 2);
+  //     ctx.fill();
+  //     xoff += sxx;
+  //   }
+  //   yoff += sxx;
+  // }
 }
 
 let lastTime = 0;
-const intervalPerSecond = 1000 / 30;
+const intervalPerSecond = 1000 / 1000;
 
 function run(currentTime) {
   if (intervalPerSecond <= currentTime - lastTime) {
     this.lastTime = currentTime;
     render();
-    return
+    // return
   }
+  if (cont === 50) return;
   requestAnimationFrame(run);
 }
 
